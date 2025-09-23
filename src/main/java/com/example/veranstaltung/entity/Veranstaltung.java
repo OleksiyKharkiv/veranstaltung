fb189a466db736950bdd0a78316a42bdfe6f38c3
OKh <hudoshin7605@gmail.com>
2025-09-23 10:23:19 +0200

    Add Veranstaltung.java

--- src/main/java/com/example/veranstaltung/entity/Veranstaltung.java
+++ src/main/java/com/example/veranstaltung/entity/Veranstaltung.java
@@ -1,20 +1,18 @@
 package com.example.veranstaltung.entity;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
 
-@Entity
-NoArgsConstructor
+
+
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @Table(name = "veranstaltung")
-
 public class Veranstaltung {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
