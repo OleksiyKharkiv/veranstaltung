1c68e107049ea5f8265046a70f846acb0804d3da
OKh <hudoshin7605@gmail.com>
2025-09-23 11:42:33 +0200

    Add Akteur.java

---
+++ src/main/java/com/example/veranstaltung/entity/Akteur.java
@@ -0,0 +1,24 @@
+package com.example.veranstaltung.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "akteur")
+public class Akteur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "akteur_id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+}
